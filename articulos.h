78ea640ca74b92bbb4d9d4e6eabe2476e335a1a9
shaulapg <149414814+shaulapg@users.noreply.github.com>
2023-11-16 04:35:10 -0600

    Add files via upload

--- articulos.h
+++ articulos.h
@@ -3,22 +3,23 @@
 class Articulo{
     private:
     std::string nombre;
-    int existencia, codigo;
+    int existencia, codigo, art=1;
     float pcompra, pventa;
 
     public:
     Articulo(): nombre(""), existencia(0), codigo(0), pcompra(0), pventa(0){};
-    Articulo(std::string nom, int exs, int cod, float pcomp, float pvent): nombre(nom), existencia(exs), codigo(cod), pcompra(pcomp), pventa(pvent){};
+    Articulo(std::string nom, int exs, float pcomp, float pvent): nombre(nom), existencia(exs), codigo(art), pcompra(pcomp), pventa(pvent){};
+    Articulo(std::string nom, float pcomp, float pvent): nombre(nom), existencia(0), codigo(art), pcompra(pcomp), pventa(pvent){};
 
     std::string get_nombre();
     int get_existencia();
-    int get_codigo();
+    int get_codprod();
     float get_pcompra();
     float get_pventa();
 
     void set_nombre(std::string);
     void set_existencia(int);
-    void set_codigo(int);
+    void set_codprod(int);
     void set_pcompra(float);
     void set_pventa(float);
     void compra(int cant);
@@ -33,7 +34,7 @@ int Articulo::get_existencia(){
     return existencia;
 }
 
-int Articulo::get_codigo(){
+int Articulo::get_codprod(){
     return codigo;
 }
 
@@ -46,14 +47,14 @@ float Articulo::get_pventa(){
 }
 
 void Articulo::set_nombre(std::string nom){
-    nombre =  nom;
+    nombre = nom;
 }
 
 void Articulo::set_existencia(int exs){
     existencia = exs;
 }
 
-void Articulo::set_codigo(int cod){
+void Articulo::set_codprod(int cod){
     codigo = cod;
 }
 
